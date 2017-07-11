7810d35c4ac9736262549c2c389f8c2e95a64a01
sunil kumar <sunilssaharan@gmail.com>
2017-07-11 23:40:36 +0000

    facing error of fultext index matching not found tring to resolve it

--- pset8/public/search.php
+++ pset8/public/search.php
@@ -6,7 +6,8 @@
     $places = [];
 
     // TODO: search database for places matching $_GET["geo"], store in $places
-    $places = CS50::query("SELECT * FROM places WHERE postal_code = ?", $_GET["geo"]);
+    $places = CS50::query("SELECT * FROM places WHERE MATCH(postal_code, place_name, admin_name1, admin_name2) AGAINST (?)", $_GET["geo"]);
+    
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
     print(json_encode($places, JSON_PRETTY_PRINT));
