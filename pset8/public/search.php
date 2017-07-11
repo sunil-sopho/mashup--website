bfae60788053b58a46efd5656d4408075e5f84ea
sunil kumar <sunilssaharan@gmail.com>
2017-07-11 23:28:59 +0000

    testing search.php on basic sql querys then improve over it

--- pset8/public/search.php
+++ pset8/public/search.php
@@ -6,7 +6,7 @@
     $places = [];
 
     // TODO: search database for places matching $_GET["geo"], store in $places
-
+    $places = CS50::query("SELECT * FROM places WHERE postal_code = ?", $_GET["geo"]);
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
     print(json_encode($places, JSON_PRETTY_PRINT));
