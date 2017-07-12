aaf0c47eae5f8943fc8393a464f2ff61d7c3af6a
sunil kumar <sunilssaharan@gmail.com>
2017-07-12 00:02:21 +0000

    made it usable for fields postal_code,postal_name,admin_name1,admin_name2 but facing error in some case for unexpected character in admin_name2

--- pset8/public/search.php
+++ pset8/public/search.php
@@ -6,7 +6,7 @@
     $places = [];
 
     // TODO: search database for places matching $_GET["geo"], store in $places
-    $places = CS50::query("SELECT * FROM places WHERE MATCH(postal_code, place_name) AGAINST (?)", $_GET["geo"]);
+    $places = CS50::query("SELECT * FROM places WHERE MATCH(postal_code, place_name,admin_name1,admin_name2) AGAINST (?)", $_GET["geo"]);
     
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
